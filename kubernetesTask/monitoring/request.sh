f4df35a1f03cc9dbc4e68e4a4801cd9e451c8f4f
VineetBavniya180 <vineet.bavniya@clouddrove.com>
2025-09-03 21:01:27 +0530

    updates files

--- kubernetesTask/monitoring/request.sh
+++ kubernetesTask/monitoring/request.sh
@@ -1,13 +1,13 @@
 #!/bin/bash 
 
-req="http://172.18.0.3:31001"
+req="http://172.18.0.2:31001"
 
-list=("/", "/about", "/slow", "veryslow", "/random-delay")
+list=("/" "/about" "/slow" "/very-slow" "/random-delay")
 
 while true; do
   for endpoint in "${list[@]}"; do
     echo "Requesting $req$endpoint"
-    curl -s "$req$endpoint"
+    curl --head -X GET "$req$endpoint" | grep "HTTP/"
     echo -e "\n"
     sleep 1
   done
